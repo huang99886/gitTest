9eed3a5bd58d3efd09b7c21543b02ae7fd914407
hct <hct@qq.com>
2023-08-04 18:43:08 +0800

    first commit

---
+++ src/test/java/com/hct/git/GitApplicationTests.java
@@ -0,0 +1,15 @@
+package com.hct.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+        System.out.println("hello git");
+    }
+
+}
